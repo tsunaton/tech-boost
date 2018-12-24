94ae5dd6eeac98580d4d79751f9fb8211aa3df98
sooryung lee <tsuna.tonight@gmail.com>
2018-12-24 13:35:56 +0900

    Rails06 ログイン機能の実装_課題2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(session_params)
-    if user&&user.authenticate(session_params)
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
       log_in user
-      redirect_to :topics, success: "ログインしました"
+      redirect_to topics_path, success: 'ログインに成功しました'
     else
-      flash.now[:danger] = "ログインに失敗しました"
+      flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
   end
@@ -20,9 +20,10 @@ class SessionsController < ApplicationController
 
   private
 
-    def session_params
-      params.require(:session).permit( :email, :password )
-    end
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+
     def log_in(user)
       session[:user_id] = user.id
     end
