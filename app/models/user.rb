ac3fd50a8049c1a93d0358da6e65f36a9c428c21
sooryung lee <tsuna.tonight@gmail.com>
2018-12-24 12:19:19 +0900

    Rails05 User新規登録に潜む問題を解決していく_課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
-  validates :email, presence:true
-  validates :password, length: { in: 8..32 }
+  validates :email, presence:true, format: { with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\z/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,32}\z/ }
+
 
   has_secure_password
 
